aeef465d5a3312fbe52d8e764e9ff25d76e9a727
ToM <tomtom637@gmail.com>
2018-06-27 16:40:29 +0200

    header media queries OKAY

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // Hero height = calc(100% - 123px)
-document.querySelector(".hero").style.height = window.innerHeight - 123 + 'px';
+function resize() {
+  document.querySelector(".hero").style.height =
+    window.innerHeight - 123 + "px";
+}
+resize();
+window.onresize = () => resize();
