275eace86820e04220cd653ad7aa624f115840fc
ToM <tomtom637@gmail.com>
2018-07-08 00:38:57 +0200

    added a map section & a footer

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ function resize() {
 }
 resize();
 window.onresize = () => resize();
+
+// Map
+function initMap() {
+  var myLatLng = { lat: 43.552542, lng: 3.892718 };
+  // Create a map object and specify the DOM element
+  // for display.
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: myLatLng,
+    zoom: 12
+  });
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng,
+    title: "Funérarium de Lattes"
+  });
+}
